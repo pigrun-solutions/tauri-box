1c82d419d346fc5412f0f8411e80241ace9dfa51
Kazizo <kazizohajar55@gmail.com>
2024-04-24 16:48:25 +0300

    Save milestone

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,57 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useEffect, useState } from 'react'
+import { Button } from './components/ui/button'
+import Database from 'tauri-plugin-sql-api'
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+interface Additive {
+    id: string
+    name: string
+    costKg: number
+    densityGmCc: number
+    createdAt: string
+    updatedAt: string
 }
 
-export default App;
+const App = () => {
+    const [additives, setAdditives] = useState<Additive[] | undefined>()
+    useEffect(() => {
+        const checkDb = async () => {
+            const db = await Database.load('sqlite:test.db')
+
+            await db.execute(
+                `CREATE TABLE IF NOT EXISTS Additives (id TEXT PRIMARY KEY,  name TEXT UNIQUE, costKg REAL DEFAULT 0.00, densityGmCc REAL DEFAULT 1.00, createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP)`
+            )
+
+            const rows = (await db.select('SELECT * FROM Additives')) as Additive[]
+            setAdditives(rows)
+        }
+
+        checkDb()
+    }, [])
+
+    console.log(additives)
+
+    return (
+        <div className="h-screen w-full bg-muted/40">
+            Add new additives
+            <Button
+                onClick={async () => {
+                    const db = await Database.load('sqlite:test.db')
+                    await db.execute(`INSERT INTO Additives (id, name) VALUES ('${Math.random()}', 'Additive ${Math.random()}')`)
+                    const rows = (await db.select('SELECT * FROM Additives')) as Additive[]
+                    setAdditives(rows)
+                }}>
+                Add
+            </Button>
+            <div>
+                {additives &&
+                    additives?.map((additive: any) => (
+                        <div key={additive.id}>
+                            {additive.name} - {additive.costKg}
+                        </div>
+                    ))}
+            </div>
+        </div>
+    )
+}
+
+export default App
