2f13ac33cd856b3b6fdf99549393098ae1655ab8
Kazizo <kazizohajar55@gmail.com>
2024-05-13 13:58:25 +0300

    Hollywood!

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,12 +1,14 @@
-import { Dialog, DialogContent } from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 
 interface ModalProps {
+    title?: string
+    description?: string
     isOpen: boolean
     onClose: () => void
     children?: React.ReactNode
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }: ModalProps) => {
+const Modal: React.FC<ModalProps> = ({ title, description, isOpen, onClose, children }: ModalProps) => {
     const onChange = (open: boolean) => {
         if (!open) onClose()
     }
@@ -14,6 +16,12 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }: ModalProps) 
     return (
         <Dialog open={isOpen} onOpenChange={onChange}>
             <DialogContent className="pb-2">
+                {title && (
+                    <DialogHeader>
+                        <DialogTitle>{title}</DialogTitle>
+                        {description && <DialogDescription>{description}</DialogDescription>}
+                    </DialogHeader>
+                )}
                 <div>{children}</div>
             </DialogContent>
         </Dialog>
